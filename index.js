ed17b776d94d9fed642a76f798546602758550c9
marcus110379 <marcusjmadden@gmail.com>
2019-08-15 12:29:29 -0500

    foreachmap

---
+++ index.js
@@ -0,0 +1,18 @@
+//Ask the user to enter a word. Keep asking them to enter a word until they enter the letter 'q'.
+
+// var userInput = prompt("enter a word");
+// while(userInput !== "q"){
+//     userInput = prompt("enter a word");
+// }
+
+//Make any string array all lower case strings. If you don't remember how to use that Google it.
+
+//var names = ["Horse", "Cars", "Buses"];
+
+var array1 = ["Horse","Cars","Buses","Houses","Planes"];
+array1 = array1.map(function(eachElement){
+    return eachElement.toLowerCase() });
+console.log(array1);
+
+
+
